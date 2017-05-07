6983958000473496f07cedd8bd4c97bfbf2c1c93
tomvh123 <thaaster@avans.nl>
2017-05-07 20:20:09 +0200

    recipes toegevoegd

--- MyFirstMochaProject/routes/routes_api_v2.js
+++ MyFirstMochaProject/routes/routes_api_v2.js
@@ -1,6 +1,7 @@
 //API version 2
 var express = require('express');
 var router = express.Router();
+var testjson = require('../recipes.js');
 
 
 router.get('/info', function(request, response){
@@ -10,12 +11,18 @@ router.get('/info', function(request, response){
     })
 })
 
+router.get('/recipes', function(request, response){
+    response.json(testjson);
+})
+
 router.get('*', function(request, response){
     response.status(404);
     response.json({
         "melding": "error 404 not-found"
     })
 })
+
+
 
 
 
