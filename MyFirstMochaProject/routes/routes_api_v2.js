7b5250156359cbce92217c8f030c45cff0dd46ea
tomvh123 <thaaster@avans.nl>
2017-05-07 21:10:59 +0200

    filters

--- MyFirstMochaProject/routes/routes_api_v2.js
+++ MyFirstMochaProject/routes/routes_api_v2.js
@@ -1,7 +1,7 @@
 //API version 2
 var express = require('express');
 var router = express.Router();
-var testjson = require('../recipes.js');
+var categorien = require('../recipes.js');
 
 
 router.get('/info', function(request, response){
@@ -11,8 +11,16 @@ router.get('/info', function(request, response){
     })
 })
 
-router.get('/recipes', function(request, response){
-    response.json(testjson);
+router.get('/recipes/:category', function(request, response, next){
+    var category = request.params.category || '';
+
+    var cat = categorien.filter(function (u) {
+        return (u.category === category);
+    })
+    console.log(cat);
+
+    response.json(cat);
+
 })
 
 router.get('*', function(request, response){
